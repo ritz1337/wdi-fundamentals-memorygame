73c1e8bcb5c8619147bda2251fbc7a1e9582b926
Ritwik Ruia <ritwikruia@ritwik-ruias-macbook-pro.local>
2016-11-27 23:46:00 -0800

    Memory Game Completed

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,6 +5,11 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";*/
 
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+
 /* 
 if (cardTwo === cardFour) {
 	alert("You found a match!");
@@ -31,11 +36,41 @@ else {
 var gameBoard = document.getElementById('game-board');
 
 	function createCards() {
-		for (i=0; i<4; i++) {
+		for (i=0; i<cards.length; i++) {
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
 		gameBoard.appendChild(cardElement);
+
 		}
 	}
+
+function isTwoCards() {
+	  cardsInPlay.push(this.getAttribute('data-card'));
+	  console.log(this.getAttribute('data-card'));
+	  if(this.getAttribute('data-card') === 'king'){
+	  	this.innerHTML = '<img src = "King_of_Hearts.png" alt="king of hearts">';
+	  }
+	  else{
+	  	this.innerHTML = '<img src = "Queen_of_Hearts.png" alt="queen of hearts">';	
+	  }
+	  	
+}
+
+if (cardsInPlay.length === 2){
+	isMatch(cardsInPlay);
+	cardsInPlay = [];
+}
+
+var isMatch = function(cards) {
+	if (cards[0] === cards[1]){
+		alert("You Found a Match!");
+	}
+	else {
+		alert("No Match, Try Again");
+	};
+
+}
 
 createCards();
